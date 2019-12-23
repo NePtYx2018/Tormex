8b21bc0d348ce1a69fe69a6372432cf97ff38a2d
NePtYx2018 <52055199+NePtYx2018@users.noreply.github.com>
2019-12-23 14:36:04 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Lo sentimos por hacerte esperar,pero Tormex se"
+echo "esta descomprimiendo..."
+unzip Tormex.zip
+mkdir $HOME/Status
+mv Tormex $HOME/Status
+rm -fr $HOME/Tormex
+mv $HOME/Status/Tormex $HOME/
+rm -fr $HOME/Status
+
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Gracias por descargar Tormex pasate por"
+echo "mi canal ayi subo cosas interesantes:"
+echo "[https://youtu.be/UrOBPSU6u-8]"
+echo ""
+
